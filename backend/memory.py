6dfec5f1922c53489aef51e7a929191b1f97b063
Aravindhan007-git <aravindhanguraj15143@gmail.com>
2026-04-11 16:14:52 +0530

    created frontend

--- backend/memory.py
+++ backend/memory.py
@@ -2,12 +2,14 @@ from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
 import os
 from dotenv import load_dotenv
 from langchain_community.vectorstores import Chroma
-from transformers import BertTokenizer
+
 load_dotenv(override=True)
+
 embedded = GoogleGenerativeAIEmbeddings(
     google_api_key=os.getenv("GOOGLE_API_KEY"),
     model="models/gemini-embedding-001"
 )
+
 db = Chroma(
     embedding_function=embedded,
     persist_directory="./persistant_memory",
@@ -17,7 +19,6 @@ db = Chroma(
 db.add_texts([
     "Ml is a launguage to train model studied in artificial intelligence and machine learning course",
     "Ritik Wake up at 6AM daily and sleep at 9PM",
-
 ])
 
 result = db.similarity_search("I wake at 8Pm",k=1)
---
+++ zeno-bot/src/Morning.tsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import "./index.css";
+
+const Morning: React.FC = () => {
+  const [Time, setTime] = useState<string>("");
+
+  const handleSubmit = () => {
+      console.log(Time)
+  };
+
+  return (
+    <div className="container">
+      <div className="triangle"></div>
+      <div className="circle"></div>
+      <div className="square"></div>
+      <div className="diamond"></div>
+      <div className="star"></div>
+      <div className="shape shape1"></div>
+      <div className="shape shape2"></div>
+      <div className="shape shape3"></div>
+      <div className="tab">
+      <h1>We’ll use this to create your daily AI schedule</h1>
+      <div className="card">
+        <div className="question">
+        <label>Wake Up Timing</label>
+        <input
+          type="time"
+          className="time-input"
+          value={Time}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTime(e.target.value)
+          }
+        ></input>
+        </div>
+        <div className="question">
+        <label>Breakfast Timing</label>
+        <input
+          type="time"
+          className="time-input"
+          value={Time}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTime(e.target.value)
+          }
+        ></input>
+        </div>
+        <div className="question">
+        <label>Morning Break Timing</label>
+        <input
+          type="time"
+          className="time-input"
+          value={Time}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTime(e.target.value)
+          }
+        ></input>
+        </div>
+        <div className="question">
+        <label>Lunch Timing</label>
+        <input
+          type="time"
+          className="time-input"
+          value={Time}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTime(e.target.value)
+          }
+        ></input>
+        </div>
+        <div className="question">
+        <label>Snacks Timing</label>
+        <input
+          type="time"
+          className="time-input"
+          value={Time}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTime(e.target.value)
+          }
+        ></input>
+        </div>
+        <div className="question">
+        <label>Evening Freshup Timing</label>
+        <input
+          type="time"
+          className="time-input"
+          value={Time}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTime(e.target.value)
+          }
+        ></input>
+        </div>
+        <div className="question">
+        <label>Dinner Timing</label>
+        <input
+          type="time"
+          className="time-input"
+          value={Time}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTime(e.target.value)   
+          }
+        ></input>
+        </div>
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Morning;
+
